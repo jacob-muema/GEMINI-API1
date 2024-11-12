372b07db2c9cd958da68624cab37bbc2bd0c21ad
Somnath Das <somnath.das@daslearning.in>
2024-11-12 16:08:24 +0530

    adding validation rule

--- terraform/ep11/variables.tf
+++ terraform/ep11/variables.tf
@@ -8,6 +8,10 @@ variable "name" {
 variable "region" {
   type        = string
   description = "The AWS region where the bucket needs to be created"
+  validation {
+    condition     = contains(["us-west1", "europe-west2", "asia-south1"], var.region)
+    error_message = "The value should be from one of the approved regions"
+  }
 }
 
 variable "project" {
