2ccb0f8939812a257a12538ac81fe7e7163a4786
DevJacob Muema <jacobmuema02@gmail.com>
2025-07-20 12:31:56 +0300

    Update txtAi.py

--- ai/googleAiStudio/python/txtAi.py
+++ ai/googleAiStudio/python/txtAi.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyDtOuEoqPTP3WFrtjVKgfm9S3Af-XQbEmw")
+genai.configure(api_key="")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("What is Reliability Engineeriing?")
 print(response.text)
